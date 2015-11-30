825ce75b7adc36b17a563947e75d6dc18ea559dd
SAUNIER DEBES Brice <brice.saunier@gmail.com>
2015-11-30 22:46:19 +0100

    App bougé en client/app Ajout du controlerAs dans le controleur

--- client/app/components/header/header-enseignant/header-enseignant-ctrl.js
+++ client/app/components/header/header-enseignant/header-enseignant-ctrl.js
@@ -4,14 +4,14 @@ angular.module( "myApp.header.headerEnseignant")
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/header-enseignant', {
             templateUrl: 'components/header/header-enseignant/header-enseignant.html',
-            controller: 'HeaderEnseignantCtrl'
+            controller: 'HeaderEnseignantCtrl',
+            controllerAs: 'header'
         });
     }])
     .controller( "HeaderEnseignantCtrl", HeaderEnseignantCtrl);
 
 function HeaderEnseignantCtrl(){
-    var vm = this;
-    vm.header = {name: "header_enseignant", url: "components/header/header-enseignant/header-enseignant.html"};
-    vm.header.name = "headerEnseignant";
-    vm.header.url = "components/header/header-enseignant/header-enseignant.html";
+    var header = this;
+    header.name = "headerEnseignant";
+    header.url = "components/header/header-enseignant/header-enseignant.html";
 }
